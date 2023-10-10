9d387039fac5ce4af1968040b45df6dd404fa4a4
Dan <bogdanlinee@gmail.com>
2023-10-10 09:14:07 +0300

    First commit

---
+++ MVC-book-library/src/index.ts
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public/book-page'));
+app.use(express.static('public/books-page'));
+
+app.listen(port, () => {
+    console.log('server is running');
+})
